f4bc8a1e0eb783006d26b9fb48465e16f79804fb
juanladino <juan.ladino@urosario.edu.co>
2020-09-14 09:40:09 -0500

    First commit

---
+++ 2. Basic Programming and Data Visualization/Ejercicios.R
@@ -0,0 +1,20 @@
+#Ejercicio condicionales, loops, funciones
+
+#Los ejercicios tienen un nivel medio y alto de dificultad, revisenlo, intenten desarrollarlos
+#En clase los solucionaremos
+
+#1. Cree un loop que reproduzca los primeros 100 numeros de la serie fibonacci
+#en la serie fibonacci, cada numero es la suma de los dos anteriores
+#ej: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ...
+
+#2. Usando loops y condicionales cree un vector de valores logicos que tome el valor de TRUE cuando un número de la lista sea par
+#usen la siguiente lista :
+lista_nums <- list(c(1, 2, 3, 4))
+#de manera que el resultado debe ser un vector o lista con los valores FALSE, TRUE, FALSE, TRUE
+
+#3. Cree una funcion que para cualquier escalar, devuelva el cuadrado del logaritmo natural de dicho escalar multiplicado por 10. 
+#Pruebe que la funcion sirve con el escalar 1 y con algun otro escalar.
+
+#4. Cree una funcion que cuente, para cualquier lista, el numero de elementos que son multiplos de 3.
+
+#5. Usando la base de datos landdata-states.csv, realice un boxplot de Home.Value y Structure.Cost por región
---
+++ 3. Basic Programming and KNN/Ejercicios KNN y Funciones.R
@@ -0,0 +1,23 @@
+#Ejercicio KNN y funciones
+
+#1. En la carpeta de la Semana 3 se encuentra un dataset de nombre winequality-red.csv
+  #Encuentren cómo importar los datos en R
+  #El dataset contiene caracteristicas de vino tinto y si nivel de calidad
+  #Una de la variables "quality" que es la variable de interes pero no es factor
+  #La variable toma los valores 3,4,5,6,7,8
+  #Deben crear una variable factor con tres categorias cat1 = 3,4, cat2 = 5,6, cat3 = 7,8
+  #Con esto listo desarrollen todo el proceso para implementar KNN
+
+
+#2. Escriban una funcion que tenga como argumento dos vectores
+  #La funcion debe sumar las posiciones respectivas de cada vector y agregar el resultado a una lista nueva
+  #Es decir, la funcion debe sumar la posicion 1 del vector1 y la posicion 1 del vector2 y este resultado agregarlo a una nueva lista
+  #Esto para cada posicion del vector
+  #La funcion debe resultar en una lista con los resultados
+
+#3. Escriban una funcion que tenga como argumento dos vectores
+  #La funcion debe sumar cada posicion del vector1 con todas las posiciones del vector2.
+  #Es decir, la funcion debe sumar la posicion 1 del vector1 y todas las posiciones del vector2
+  #Luego debe sumar la posicion 2 del vector1 con todas las posiciones del vector2 y asi sucesivamente
+  #Cada resultado debe ser agregado a una lista nueva
+  #La funcion debe resultar en una lista con los resultados
